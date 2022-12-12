f630f6dce205526772b13bdf33904a309860a9e2
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-12-13 00:24:47 +0530

    Added UC5:According to Rule 1 Password is valid or not is checked.

---
+++ src/com/Bridgelabz/UserPasswordValidation.java
@@ -0,0 +1,39 @@
+package com.Bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserPasswordValidation {
+
+	public static boolean checkValidPassword(String password) {
+		
+		String passwordregex = ".{8,}";
+		Pattern pattern = Pattern.compile(passwordregex);
+		
+		if (password == null) {
+			return false;
+		}
+		
+		Matcher matcher = pattern.matcher(password);
+		return matcher.matches();
+
+	}
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your password: ");
+		String password = sc.next();
+		
+		boolean ispasswordValid = checkValidPassword(password);
+		
+		if (ispasswordValid) {
+			System.out.println(password + " is valid");
+		}
+		else {
+			System.out.println(password + " is not valid");
+		}
+
+	}
+}
